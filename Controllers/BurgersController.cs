57040ca5d1a178d6c36857f422b81eb8bcfa3c1e
carissa morrow <carissamorrow@hotmail.com>
2019-01-14 14:04:40 -0700

    added sides/drinks

--- Controllers/BurgersController.cs
+++ Controllers/BurgersController.cs
@@ -14,9 +14,9 @@ namespace BurgerShack.Controllers
   {
     public List<Burger> Burgers = new List<Burger>()
     {
-      new Burger("Mark Burger", "A delicious burger with bacon and stuff", 7.56f),
-      new Burger("Jake Burger", "Now with fries!", 8.54f),
-      new Burger("D$ Burger", "It's Mostly Foraged", 6.24f)
+      new Burger("Carissa Burger", "With Mac and Cheese and Bourbon bbq sauce!", 7.56f),
+      new Burger("Sally Burger", "Now with fries!", 8.54f),
+      new Burger("Joe", "It's all meat", 6.24f)
     };
 
 
