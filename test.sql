9b8978bb14819b9b52a04f9758c1342ed19eb94a
carissa morrow <carissamorrow@hotmail.com>
2019-01-15 12:41:22 -0700

    playing with SQL table

--- test.sql
+++ test.sql
@@ -1,16 +1,26 @@
-CREATE TABLE burgershack (
-  Id int NOT NULL AUTO_INCREMENT,
-BurgerName VARCHAR(255),
-PRIMARY KEY (id)
-);
+-- CREATE TABLE burgershack (
+--   Id int NOT NULL AUTO_INCREMENT,
+-- BurgerName VARCHAR(255),
+-- PRIMARY KEY (id)
+-- );
 
+-- INSERT INTO burgershack (BurgerName) VALUES ("Carissa Burger");
+-- INSERT INTO burgershack (BurgerName) VALUES ("Sally Burger");
+-- INSERT INTO burgershack (BurgerName) VALUES ("Joe");
 
-INSERT INTO burgershack (BurgerName) VALUES ("Carissa Burger");
-INSERT INTO burgershack (BurgerName) VALUES ("Sally Burger");
-INSERT INTO burgershack (BurgerName) VALUES ("Joe");
-
---selects all tata from table
+--selects all data from table (get all)
 -- SELECT * FROM burgershack;
 
 --deletes table and all data
 -- DROP TABLE burgershack;
+
+-- get by id 
+--SELECT * FROM burgershack WHERE id = 1
+
+  -- UPDATE burgershack SET 
+  -- NAME = "Carissa Burger"
+  -- WHERE id  = 1;
+  -- SELECT * FROM burgershack WHERE id = 1;
+
+--delete from burgershack
+--DELETE FROM burgershack WHERE id = 1;
